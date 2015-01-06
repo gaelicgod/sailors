e30f0a6a322d97c9de866cb05ad2389216e9a67e
Bevan Christians <gaelicgod@gmail.com>
2015-01-06 20:08:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sailors
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/BoatController.js
@@ -0,0 +1,11 @@
+/**
+ * BoatController
+ *
+ * @description :: Server-side logic for managing boats
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
